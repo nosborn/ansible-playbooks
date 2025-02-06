cac133c7ecbeab15698b308c0276c4e825555fd6
Nick Osborn <nosborn@users.noreply.github.com>
2025-02-06 12:33:45 +0800

    Media server

--- .pre-commit-hooks.d/ansible-vault-encrypted.sh
+++ .pre-commit-hooks.d/ansible-vault-encrypted.sh
@@ -6,10 +6,10 @@ set -o nounset
 result=0
 
 for file in "$@"; do
-  if [[ $(head -n 1 "${file}") != '$ANSIBLE_VAULT;1.1;AES256' ]]; then
+  if [[ $(head -n 1 "${file}") != "\$ANSIBLE_VAULT;1.1;AES256" ]]; then
     printf '%s is not encrypted\n' "${file}" >&2
     result=1
   fi
 done
 
-exit ${result}
+exit "${result}"
